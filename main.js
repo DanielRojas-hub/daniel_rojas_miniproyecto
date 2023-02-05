50c92cb3e9c985838090329caae86167d1ed23df
DanielRojas-hub <69920982+DanielRojas-hub@users.noreply.github.com>
2023-02-05 09:37:20 -0400

    carousel creation

--- main.js
+++ main.js
@@ -31,3 +31,46 @@ function linkAction() {
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== HERO ====================*/
+const slider = document.getElementById('slider');
+let sliderSection = document.querySelectorAll('.slider__section');
+let lastSliderSection = sliderSection[sliderSection.length - 1];
+
+const btnLeft = document.getElementById('btn-left');
+const btnRight = document.getElementById('btn-right');
+
+slider.insertAdjacentElement('afterbegin', lastSliderSection);
+
+function next() {
+    let sliderSectionFirst = document.querySelectorAll('.slider__section')[0];
+    slider.style.marginLeft = "-200%";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function() {
+        slider.style.transition = "none";
+        slider.insertAdjacentElement("beforeend", sliderSectionFirst);
+        slider.style.marginLeft = "-100%";
+    }, 700)
+}
+
+function prev() {
+    let sliderSection = document.querySelectorAll('.slider__section');
+    let lastSliderSection = sliderSection[sliderSection.length - 1];
+    slider.style.marginLeft = "0";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function() {
+        slider.style.transition = "none";
+        slider.insertAdjacentElement("afterbegin", lastSliderSection);
+        slider.style.marginLeft = "-100%";
+    }, 700)
+}
+
+btnRight.addEventListener('click', function() {next()})
+
+btnLeft.addEventListener('click', function() {prev()})
+
+/*====== TIMER ======*/
+
+let temp = setInterval(() => {
+    next()
+}, 5000);
